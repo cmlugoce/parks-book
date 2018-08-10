6aa2c44164b9ab2a96766b53ac6c08b5e97a2a71
cmlugoce <cmlugoce@syr.edu>
2018-08-10 14:42:53 -0700

    update file

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,2 +1,56 @@
 class TrailsController < ApplicationController
-end
+    before_action :set_trail, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @trails = Trail.all
+    end
+  
+    def new
+      @trail = Trail.new
+  
+    end
+  
+    def show
+     
+    end
+  
+    def create
+      @trail = Trail.new(trail_params)
+      if @trail.save!
+        redirect_to trails_path(@trail)
+  
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      @trail.update(trail_params)
+      if @trail.save
+        redirect_to trails_path(@trail)
+      else
+        render :edit
+      end
+  
+    end
+  
+    def destroy
+      @trail.destroy
+      redirect_to park_trails_path
+  
+    end
+  
+    private
+  
+    def set_trail
+      @trail = Trail.find_by_id(params[:id])
+  
+    end
+  
+    def trail_params
+      params.require(:trail).permit(:name, :image, :distance, :date, :description, :user_id, :park_id )
+    end
+  end
