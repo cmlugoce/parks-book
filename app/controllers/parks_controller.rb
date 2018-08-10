aa3e9b95b64c38367261e8e97b04ccbee66ccffe
cmlugoce <cmlugoce@syr.edu>
2018-08-09 20:29:16 -0700

    modify controllers and views

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,43 @@
 class ParksController < ApplicationController
+
+    def index 
+        @parks = Park.all 
+    end 
+
+    def show 
+        @park = Park.find(params[:id])
+    end 
+
+    def new
+        @park = Park.new
+    end 
+
+    def create 
+        @park = Park.new(park_params)
+        if @park.save!
+            redirect to user_park_path(@park)
+        else
+            render :new 
+        end
+    end 
+
+    def edit 
+        @park = Park.find(params[:id])
+    end 
+
+    def update
+        @park = Park.find(params[:id])
+        if @park.update!(park_params)
+        redirect to user_park_path(@park)
+        end 
+    end 
+
+    private 
+    def park_params
+        params.require(:park).permit(:name, :location)
+      end
+
+
+
+
 end
