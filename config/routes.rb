73bff83ecf82110b923ad06ff141ebf124b64e76
cmlugoce <cmlugoce@syr.edu>
2018-08-09 18:16:24 -0700

    add welcome controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :trails
-  resources :parks
-  resources :users
+  root 'welcome#index'
+  resources :users do 
+    resources :parks 
+      resources :trails 
+  end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
