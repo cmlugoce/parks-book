1cd6c60877dee833e590545cbce3642444697f8e
cmlugoce <cmlugoce@syr.edu>
2018-08-12 10:50:19 -0700

    add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :validate_user
     before_action :require_login, except: [:new, :create, :index]
 
     def current_user
@@ -16,4 +17,20 @@ class ApplicationController < ActionController::Base
     def require_login
         redirect_to root_path unless logged_in?
     end 
+
+    def user_exist
+        if params[:user_id] && !User.exists?(params[:user_id])
+          render ':welcome/index', message: "User not found"
+        end
+      end
+    
+      def match_user
+        if params[:user_id] != current_user.id.to_s
+          render ':welcome/index', message: "User ID authentication failure. You cannot edit other user's information."
+        end
+      end
+    
+      def validate_user
+        user_exist && match_user
+      end
 end
