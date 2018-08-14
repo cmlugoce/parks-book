321e7c72b5eea139fb582b446fb4d4e2e2161434
cmlugoce <cmlugoce@syr.edu>
2018-08-14 16:33:57 -0700

    update params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
       private 
     
       def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:name, :email, :password)
       end 
     
 end
