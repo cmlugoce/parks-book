795d617c714ff1455f34e4185011d02f3fb2edeb
cmlugoce <cmlugoce@syr.edu>
2018-08-22 19:13:16 -0700

    delete  unused attributes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :logged_in?
   before_action :validate_user
-      
+  
       def create
         @trail = Trail.find(params[:trail_id])
         @comment = @trail.comments.new(comment_params)
@@ -14,14 +14,15 @@ class CommentsController < ApplicationController
           redirect_to trail_path(@trail)
         end
       end
-
+     
+     
       
  private
  
 
 
  def comment_params
-    params.require(:comment).permit(:body, :user_id, :trail_id, user_attributes:[:name])
+    params.require(:comment).permit(:body, :user_id, :trail_id)
   
  end 
 end 
