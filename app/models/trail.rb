0e729bd7cb69ea200c3a5f6564dda1bb87245f67
cmlugoce <cmlugoce@syr.edu>
2018-08-22 19:15:19 -0700

    add validation

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -2,10 +2,10 @@ class Trail < ApplicationRecord
   
   belongs_to :park
   belongs_to :user
- has_many :comments
+ has_many :comments, :dependent => :destroy
  
  mount_uploader :image, ImageUploader
- validates_presence_of :name, :distance
+ validates_presence_of :name, :distance, :date, :difficulty
 
 
  
