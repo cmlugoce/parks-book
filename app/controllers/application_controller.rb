ebd5d8fb202714c909815c857a099ca691495577
cmlugoce <cmlugoce@syr.edu>
2018-08-22 19:30:40 -0700

    delete unused methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user
     helper_method :logged_in?
-    helper_method :validate_user
     before_action :require_login, except: [:new, :index]
 
     def current_user
@@ -18,19 +17,5 @@ class ApplicationController < ActionController::Base
         redirect_to root_path unless logged_in?
     end 
 
-    def user_exist
-        if params[:user_id] && !User.exists?(params[:user_id])
-          render ':welcome/index', message: "User not found"
-        end
-      end
     
-      def match_user
-        if params[:user_id] != current_user.id.to_s
-          render ':welcome/index', message: "User ID authentication failure. You cannot edit other user's information."
-        end
-      end
-    
-      def validate_user
-        user_exist && match_user
-      end
 end
