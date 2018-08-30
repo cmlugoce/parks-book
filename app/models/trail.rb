4cfd7c1915ba0d91c81359fc4471e5fe53125ba7
cmlugoce <cmlugoce@syr.edu>
2018-08-30 10:47:37 -0700

    add next, previous methods

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,7 +1,6 @@
 class Trail < ApplicationRecord
   
   belongs_to :park
-  #belongs_to :user
   has_many :users, through: :comments
   has_many :comments, :dependent => :destroy
  
@@ -18,5 +17,21 @@ end
 def self.search2(search)
   where("difficulty LIKE ? ", "#{search}")
 end
+def next
+  # if the first destination is greater the current one exists then return the next destination
+  if next_trail = self.class.where("id > ?", id).first
+    next_trail
+  else
+   Trail.first
+  end
+end
 
+def previous
+  # if the destination is less then current one exists then return the previous destination
+  if previous_trail = self.class.where("id < ?", id).last
+    previous_trail
+  else
+    Trail.last
+  end
+end
 end
