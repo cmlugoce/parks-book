a225f81a7fa65bf0e01e7578a399bd0c67fe0069
cmlugoce <cmlugoce@syr.edu>
2018-08-31 15:17:38 -0700

    add comment #destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,34 @@
 class CommentsController < ApplicationController
   before_action :require_login
+ # before_action :set_trail
   
       def create
         @trail = Trail.find(params[:trail_id])
         @comment = @trail.comments.new(comment_params)
       
         if @comment.save!
-           render json: @comment, status: 201
+          #format.html { redirect_to @trail, flash[:notice] 'Comment was successfully created.' }
+          format.json { render json: @comment, status: 201}
         else
-          render json: {errors: @comment.errors.full_messages}, status: 400
+          #format.html { redirect_to @trail, notice: "You can't leave the comment box blank.Please try again!" }
+          format.json { render json: @comment.errors, status:400 }        
         end
       end
      
-     
+      def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        respond_to do |format|
+          format.html { redirect_back fallback_location: root_path, notice: 'Comment was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
       
  private
  
+ def set_trail
+  @trail = Trail.find(params[:trail_id])
+ end 
 
 
  def comment_params
