1124d747592e42b34ef22f07210116270d237b5e
cmlugoce <cmlugoce@syr.edu>
2018-09-02 21:02:08 -0700

    add association

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,6 +1,7 @@
 class ParkSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :image
   has_many :trails
+  has_one :user
   def image
 		return object.image.url(:medium)
 	end
