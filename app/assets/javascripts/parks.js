8c8c261b8aa852a963f7e3627def116878900f0d
cmlugoce <cmlugoce@syr.edu>
2018-09-07 13:58:43 -0700

    add sort function

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,66 +1,8 @@
 
-///////////truncate parks index////////////////
-$(document).on('click', ".js-more", function(event) {
-  event.preventDefault();
-    let id = $(this).attr("data-id");
-   // debugger
-    $.get("/parks/" + id + ".json", function(park) { 
-   $("#location-" + id).html(park.location);
- });
-});
 
 
-////////////View trails on 'parks/show'//////////////
 
-$(function() {
-  $(".js-view-trails").on('click',  function(event) {
-    //debugger
-  //  let id = $(".js-view-trails").attr("data_id"); 
-   let id = event.target.getAttribute("data_id");
- //  debugger
-    $.get("/parks/" + id + ".json", function(data) {
-     // debugger
-      // pass data to loadUserTrails
-      loadUserTrails(data);
-    })
-    event.preventDefault();
-  }.bind(this))
-});
-function loadUserTrails(data) {
 
- let trails = data["trails"];
- let usersTrailsDiv = $(".parksTrails");
- debugger
-// empty the div first
-usersTrailsDiv.empty();
-//debugger
-// set json to variables
-let trailID = data["id"];
-let name = data["name"];
-let image = data["image"];
-//debugger
-
-// iterate over each  in the  array
-$.each (trails, function(index, trail) {
-//debugger
-  usersTrailsDiv.append(
-    `<div class='row'>
-      <div class='col-sm-8'>
-        <h3><a href='/trails/${trail.id}'>${trail.name}</a></h3>
-        <h4>Difficulty: ${trail.difficulty}</h4>
-        </div>
-        <div class='col-sm-4'>
-         
-            <img src='${trail.image}' alt='testing'>
-          
-        </div>
-    </div>
-    <hr>`
-    
-  )
-  //return false
-});
-}
 
 /////////////////////view next/prev/////////////////////
 
@@ -86,7 +28,7 @@ $(function(){
   $.get("/parks/" + id + "/next", function(data) {
   // console.log(data)
     debugger
-   loadPark(data);
+   loadPark(data); //declaration
    
   });
   event.preventDefault();
@@ -99,7 +41,7 @@ function loadPark(data) {
   // change the URL to the new route
   history.pushState({}, "", "/parks/" + data.id)
   
-  // re-set the id to current on the buttons
+  // reset the id to current on the buttons
   $(".js-next-park").attr("data-id", data["id"]);
   $(".js-previous-park").attr("data-id",data["id"]);
   
@@ -107,7 +49,7 @@ debugger
 
   let parkHeader = $(".park-header")
   let parkPage = $("#parkImage")
-  let parkTrail = $("#park")
+  
   // empty the div
 
   parkHeader.empty()
@@ -118,12 +60,11 @@ debugger
   "<h3 class='item1'>"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Location: &nbsp;&nbsp;&nbsp;"+(data["location"])+"</h3>"
    + "</center>"  
 )
+  // empty the div
   
-  //$.each (trailss, function(index, trail){
   parkPage.empty()
   parkPage.append(
-    //"<div class='container'>"+
-    // "<div class='row'>"+
+    
      
      
    " <br></br>" +
@@ -133,54 +74,31 @@ debugger
 "</div>"
 )
     
-//let user_cur = current_user.parks.include?(@park)
+
 
 let trails = (data["trails"])
 
-let userName = data["user"]["name"]
-let parkID = data["id"]
-let userID = data["user"]["id"]
-
+//let userName = data["user"]["name"]
+//let parkID = data["id"]
+//let userID = data["user"]["id"]
+let parkTrail = $("#park")
 debugger
 
-ParkTrail.empty()
+parkTrail.empty()
 $.each (trails, function(index, trail){
- //debugger
+ debugger
  
   parkTrail.append(
   
- `
+ `<center>
   <h3><a href='/trails/${trail.id}'>${trail.name}</a></h3>
   </center>
   </div>
   </div>
   </div>
   </div>
+
   `
-)
-})
- 
-
- // parkPage.append (
-
-   // `    <div class='row'>
-   // <div class='col-md-5'>
-   // <img src='${data.image}'>
-    
-  /// </div>`
-    //<div class='col-md-5'>
-      //<h3><a href='/parks/${parkID}/edit'>Edit</a></h3>
-        //    <h3><a href='/parks/${parkID}/delete'>Delete</a></h3>
-        //    <h3><a href='/parks/${parkID}/trails/new'>Add new trails</a></h3>
-        //    </div>
-            
-             
-   // `
-  //)
-  
-  
-
-
-
-
+    )  
+   })
 }
