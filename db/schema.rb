3f421e32b441baa2dfd5760e1fcc0c59f8ceb3e5
cmlugoce <cmlugoce@syr.edu>
2019-01-12 09:04:03 -0800

    sqlite to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_26_191601) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "park_id"
     t.integer "user_id"
